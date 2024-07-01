280f385b0c3bc9b5ab2a7eb27451d367497fbe99
Abdirahman004 <abdirahman.alio@student.moringaschool.com>
2024-07-01 17:28:53 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+// Assuming returnFirstTwoDrivers and returnLastTwoDrivers are defined as:
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+  };
+  
+  // selectingDrivers
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // createFareMultiplier
+  function createFareMultiplier(multiplier) {
+    return function(fare) {
+      return fare * multiplier;
+    };
+  }
+  
+  // fareDoubler
+  const fareDoubler = createFareMultiplier(2);
+  
+  // fareTripler
+  const fareTripler = createFareMultiplier(3);
+  
+  // selectDifferentDrivers
+  function selectDifferentDrivers(drivers, selectFunction) {
+    return selectFunction(drivers);
+  }
+  
